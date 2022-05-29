f31b1530ef95fc290756804efb02b86bdb0f981a
DOSZ <DOSZ@DESKTOP-TPFAQMD>
2022-05-28 23:24:53 -0500

    Add project files.

---
+++ FrontendBiblio/Data/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FrontendBiblio.Data 
+{
+    public class Book
+    {
+        [Key]
+        public int Page { get; set; }
+        public string Title { get; set; }
+        public string Editorial { get; set; }
+        public string Author { get; set; }
+        public int BookID { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FrontendBiblio/Data/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendbiblioteca.Modelo
+{
+    public class Student
+    {
+        [Key]
+        public int Semester { get; set; }
+        public string Name { get; set; }
+        public string Carreer { get; set; }
+        public int IDStudent { get; set; }
+        public int Loans { get; set; }
+    }
+}
